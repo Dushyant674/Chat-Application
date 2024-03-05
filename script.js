94dcd644b58636c574409a0768a01503a24b4c7a
Dushyant <dushyantkumar674@gmail.com>
2024-03-05 16:21:04 +0530

    Final Update

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ messageInput = document.querySelector('#messageInp'),
 messageContainer = document.querySelector('.msg-container'),
 uName = document.querySelector('#yourName'),
 userlist = document.querySelector('.user-container'),
+typingUser = document.querySelector('.typer')
 submitBtn = document.querySelector('#submitbtn'),
 audio = new Audio('/assets/audio/ting.mp3');
 
@@ -15,7 +16,6 @@ const givename = ()=>{
     uName.innerHTML = `<span class="active"></span>${splitUrl} (You)`
     return splitUrl;
 }
-
 let userId = givename();
 
 // create the list of active users 
@@ -28,6 +28,18 @@ const active = (user)=>{
     }
 }
 
+const getCurrentTime = ()=>{
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+
+    let currentTimeString = `${hours}:${minutes}`;
+    return currentTimeString;
+}
+
 // add the message received from other user's on our chat page
 const append = (message, position) => {
     const messageElement = document.createElement('div');
@@ -35,19 +47,26 @@ const append = (message, position) => {
     messageElement.classList.add('msg');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
+    const messtime = document.createElement('div');
+        messtime.innerHTML = getCurrentTime();
+        messtime.classList.add('mtime');
+        messageElement.append(messtime)
     if (position == 'left') {
         audio.play();
     }
 }
 
+
+// if input field is blank disables the send button
 const changeSendColor = ()=>{
     if (messageInput.value.trim() == ''){
-        submitBtn.classList.remove('typing')
+        submitBtn.classList.remove('typing');
     } else (
         submitBtn.classList.add('typing')
     )
 }
 
+socket.emit('new-user-joined', userId);
 
 form.addEventListener('input', ()=>{
     socket.emit('typing', userId);
@@ -65,12 +84,18 @@ form.addEventListener("submit", (e) => {
         changeSendColor();
     }
 })
-socket.emit('new-user-joined', userId);
 
 socket.on('connected-users', onlineUsers =>{
     userlist.innerHTML = ''
     for(let i in onlineUsers)
     active(onlineUsers[i]);
+})
+
+socket.on('typing', typer =>{
+    typingUser.innerText = `${typer} is typing...`;
+    setTimeout(()=>{
+        typingUser.innerText = ''
+    }, 3000);
 })
 
 socket.on('receive', data => {
